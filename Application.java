7c0a715c60688daf38ebd19713630032809c53cf
MaxK <MaxK@DESKTOP-C4JP5G0>
2017-07-04 21:11:13 +0300

    add Class

---
+++ Application.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CalcClass;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author MaxK
+ */
+public class Application {    
+    public static void main(String[] args) throws IOException{
+        Formula calc;
+        System.out.println("Введите выражение вставляя пробел после каждого оператора");
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        calc = new Formula(reader.readLine());
+        System.out.println(calc.Estimate());
+        
+    }
+    
+}
